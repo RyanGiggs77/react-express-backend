7399c73b6ff24361996b449408310a42609ec128
RyanGiggs77 <muhammadryan.giggs@gmail.com>
2025-03-19 14:25:36 +0700

    update routes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 app.use('/auth', authRoutes);
 app.use(accountRoutes); // Perbaiki path ini
-app.use('/transactions', transactionsRoutes); // Tambahkan route ini
+app.use(transactionsRoutes); // Tambahkan route ini
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
